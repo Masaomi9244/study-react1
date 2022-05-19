5ccba4e63796cd31f369447482de7419e6342e34
まさおみ <mario8masaomiomi@gmail.com>
2022-05-19 21:20:26 +0900

    MacBookでpushできるかのテスト

--- study-react/src/pages/about.jsx
+++ study-react/src/pages/about.jsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Footer } from '../components/Footer'
-import { Main } from '../components/Main'
 import { Header } from '../components/Header'
+import { Main } from '../components/Main'
+import { Footer } from '../components/Footer'
 
 export default function Home() {
   return (
