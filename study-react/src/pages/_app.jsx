63033c0cf95b3b3366fd7b96c17606a6a4a70b0f
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-05-31 22:18:26 +0900

    🔥 不必要なコードやファイルを削除

--- study-react/src/pages/_app.jsx
+++ study-react/src/pages/_app.jsx
@@ -1,21 +1,12 @@
-import "src/styles/globals.css";
 import Head from "next/head";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgColor } from "src/hooks/useBgColor";
-import React from "react";
 
 const MyApp = ({ Component, pageProps }) => {
-  const counter = useCounter();
-  const inputArray = useInputArray();
-  useBgColor();
-
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} {...counter} {...inputArray} />
+      <Component {...pageProps} />
     </>
   );
 };
