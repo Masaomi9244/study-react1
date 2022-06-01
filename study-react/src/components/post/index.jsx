9ac90f1cbf8f6858b4bb2c2d549a01a0f72725a0
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-06-01 22:09:31 +0900

    ♻️ 不要なコードを削除

--- study-react/src/components/post/index.jsx
+++ study-react/src/components/post/index.jsx
@@ -13,11 +13,9 @@ export const Post = () => {
 
   return (
     <div>
-      <div>
-        <h1>{post?.title}</h1>
-        <p>{post?.body}</p>
-        {user?.name ? <div>Created by {user.name}</div> : null}
-      </div>
+      <h1>{post?.title}</h1>
+      <p>{post?.body}</p>
+      {user?.name ? <div>Created by {user.name}</div> : null}
     </div>
   );
 };
