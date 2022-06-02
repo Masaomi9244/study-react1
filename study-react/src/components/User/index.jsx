b42ad224b31824a5714b0c8ee812df897ca8f4c1
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-06-02 21:54:11 +0900

    🚚 フォルダ名を変更した分の差分が挙げられていなかったのでコミット

---
+++ study-react/src/components/User/index.jsx
@@ -0,0 +1,20 @@
+import { useUser } from "src/hooks/useUser";
+
+export const User = () => {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) {
+    return <div>ローディング中</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>{user?.name}</h1>
+      <p>{user?.email}</p>
+    </div>
+  );
+};
