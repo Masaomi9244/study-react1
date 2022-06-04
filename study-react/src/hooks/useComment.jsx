12a794b58d765adff2636bba3d9c0395cec5a30b
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-06-04 22:05:52 +0900

    ♻️ APIを叩く処理を別ファイルに切り分けた

---
+++ study-react/src/hooks/useComment.jsx
@@ -0,0 +1,26 @@
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
+export const useComment = () => {
+  const router = useRouter();
+  const fetcher = async (url) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("エラーが発生したため、データの取得に失敗しました");
+    }
+
+    return response.json();
+  };
+
+  const { data: comment, error: commentError } = useSWR(
+    `https://jsonplaceholder.typicode.com/comment/${router.query.id}`,
+    fetcher
+  );
+
+  return {
+    comment,
+    commentError,
+    isLoading: !comment && !error,
+    isEmpty: comment && comment.length > 0,
+  };
+};
