53dadf11dc644b4e7b3c45fc0f1432003be111bc
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-06-06 08:13:19 +0900

    🎨 commentページから別コンポーネントに切り分けたコードを削除

--- study-react/src/pages/comments/[id]/index.jsx
+++ study-react/src/pages/comments/[id]/index.jsx
@@ -1,36 +1,13 @@
-import { useRouter } from "next/router";
-import useSWR from "swr";
+import { Comment } from "src/components/Comment";
+import { Header } from "src/components/Header";
 
-const Comment = () => {
-  const router = useRouter();
-
-  const fetcher = async (url) => {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("エラーが発生しましたーーー");
-    }
-    return response.json;
-  };
-
-  const { data: comment, error: commentError } = useSWR(
-    `https://jsonplaceholder.typicode.com/comments/${router.query.id}`,
-    fetcher
-  );
-
-  if (comment && comment.length === 0) {
-    return <div>データは空です。</div>;
-  }
-
-  if (commentError) {
-    return <div>{commentError.message}</div>;
-  }
-
+const CommentId = () => {
   return (
     <div>
-      {/* <h1>{comment.body}</h1>
-      <div>{comment.email}</div> */}
+      <Header />
+      <Comment />
     </div>
   );
 };
 
-export default Comment;
+export default CommentId;
