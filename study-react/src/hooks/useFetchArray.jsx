0017a99b63dbf8ed5492b70e3793ec942402d938
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-06-09 19:02:24 +0900

    ♻️ SWRのfetchを共通化

--- study-react/src/hooks/useFetchArray.jsx
+++ study-react/src/hooks/useFetchArray.jsx
@@ -1,16 +1,7 @@
 import useSWR from "swr";
 
-const fetcher = async (url) => {
-  const response = await fetch(url);
-
-  if (!response.ok) {
-    throw new Error("エラーが発生したため、データの取得に失敗しました");
-  }
-  return response.json();
-};
-
 const useFetchArray = (url) => {
-  const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR(url);
 
   return {
     data,
